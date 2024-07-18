908f2b6e2bdfc456eb0b26ba93376915c6b02a2c
dinesh-bk <dineshbk2401@gmail.com>
2024-07-18 18:46:08 -0500

    Complete data_check

---
+++ src/basic_cleaning/run.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+"""
+Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact
+"""
+import argparse
+import logging
+import wandb
+
+import os
+import pandas as pd
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(job_type="basic_cleaning")
+    run.config.update(args)
+
+    # Download input artifact. This will also log that this script is using this
+    # particular version of the artifact
+    # artifact_local_path = run.use_artifact(args.input_artifact).file()
+
+    logger.info("Downloading the artifact.")
+    artifact = run.use_artifact(args.input_artifact)
+    artifact_local_path = artifact.file()
+
+    logger.info("Reading artifact")
+    df = pd.read_csv(artifact_local_path)
+
+    logger.info("Cleaning the data")
+    df['last_review'] = pd.to_datetime(df['last_review'])
+    idx = df['price'].between(args.min_price, args.max_price)
+    df = df[idx].copy()
+
+    filename = "clean_sample.csv"
+    df.to_csv(filename, index=False)
+
+    logger.info("Uploading preprocessed data")
+
+    logger.info("Creating the artifact")
+    artifact = wandb.Artifact(
+     args.output_artifact,
+     type=args.output_type,
+     description=args.output_description,
+    )
+    artifact.add_file(filename)
+
+    logger.info("Logging the artifact")
+    run.log_artifact(artifact)
+
+    os.remove(filename)
+
+
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="A very basic data cleaning")
+
+
+    parser.add_argument(
+        "--input_artifact", 
+        type=str,
+        help= "Data to be preprocessed",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_artifact", 
+        type=str,
+        help= "Preprocessed Data",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_type", 
+        type=str,
+        help="Output type for the artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--output_description", 
+        type=str,
+        help="Download from W&B the raw dataset and apply some basic data cleaning, exporting the result to a new artifact",
+        required=True
+    )
+
+    parser.add_argument(
+        "--min_price", 
+        type=float,
+        help="Minimum price",
+        required=True
+    )
+
+    parser.add_argument(
+        "--max_price", 
+        type=float ,
+        help="Maximum price",
+        required=True
+    )
+
+
+    args = parser.parse_args()
+
+    go(args)
