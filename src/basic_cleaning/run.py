88cb519a006a797408e522e8566303dcc5ec3732
dinesh-bk <dineshbk2401@gmail.com>
2024-07-19 11:55:51 -0500

    Modifies to succes the test proper boundaries

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -34,6 +34,10 @@ def go(args):
     idx = df['price'].between(args.min_price, args.max_price)
     df = df[idx].copy()
 
+    # To succes the test proper boundaries
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
 
